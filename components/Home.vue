3ccb07ad8d37964f37661c1fab39c50665364928
Gaëtan H <steellgold0@gmail.com>
2022-08-18 23:56:26 +0200

    Some edits

--- components/Home.vue
+++ components/Home.vue
@@ -4,6 +4,7 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
       <h1 class="text-white text-8xl font-black">Salut <span class="wave">👋🏻</span></h1>
+      <p class="text-gray-300 font-normal text-2xl">azaz</p>
     </div>
   </div>
 </template>
@@ -16,15 +17,15 @@
 
 <style>
   span.wave {
-      animation-name: wave-animation;  /* Refers to the name of your @keyframes element below */
-      animation-duration: 2.5s;        /* Change to speed up or slow down */
-      animation-iteration-count: infinite;  /* Never stop waving :) */
-      transform-origin: 70% 70%;       /* Pivot around the bottom-left palm */
+      animation-name: wave-animation;
+      animation-duration: 2.5s;
+      animation-iteration-count: infinite;
+      transform-origin: 70% 70%;
       display: inline-block;
   }
 
   @keyframes wave-animation {
-      0% { transform: rotate( 0.0deg) }
+    0% { transform: rotate( 0.0deg) }
     10% { transform: rotate(14.0deg) }
     20% { transform: rotate(-8.0deg) }
     30% { transform: rotate(14.0deg) }
