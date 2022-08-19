78709cab085f179190c91e18632485c8c69a8c1c
Gaëtan H <steellgold0@gmail.com>
2022-08-19 21:13:39 +0200

    :weary:

---
+++ src/lib/components/Bar.svelte
@@ -0,0 +1,41 @@
+<div class="nav">
+  <nav class="navbar">
+    <img class="pdp" src="/images/profile.jpg" alt="Profile Picture" width="40px" height="40px">
+
+    <div class="navbar-links">
+      <a class="navbar-link" href="/">Home</a>
+      <a class="navbar-link" href="/">Projects</a>
+      <a class="navbar-link" href="/">Contact</a>
+    </div>
+  </nav>
+</div>
+
+<style lang="scss">
+  .nav {
+    a.navbar-link {
+      font-weight: 500;
+    }
+  }
+
+  nav.navbar {
+    display: flex;
+    justify-content: center;
+  }
+
+  .navbar-links {
+    display: flex;
+    gap: 10px;
+  }
+
+  a.navbar-link {
+    justify-content: flex-end;
+    text-decoration: none;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 200;
+  }
+
+  img.pdp {
+    border-radius: 10px;
+  }
+</style>
