59ba28c2bf81c67ac7b81cb112f4b0bb6c9bddd1
Gaëtan H <steellgold0@gmail.com>
2022-08-19 16:05:34 +0200

    Svelte is amazing

---
+++ src/lib/components/Nested.svelte
@@ -0,0 +1,10 @@
+<h3>Hey</h3>
+
+<style lang="scss">
+  @import "../scss/variables.scss";
+
+  h3 {
+    background-color: $color-primary;
+    color: black;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="center">Salut <span class="wave">👋🏻</span></h1>
+
+<style>
+  @media screen and (max-width:480px) {
+    .center {
+      position: absolute;
+      top: 30%;
+      left: 35%;
+      margin-top: -50px;
+      margin-left: -50px;
+      font-size: 60px;
+    }
+  }
+
+  @media screen and (min-width:480px) {
+    .center {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin-top: -50px;
+      margin-left: -50px;
+    }
+  }
+
+  h1 {
+    text-align: center;
+  }
+</style>
