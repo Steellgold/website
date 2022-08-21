d2c675cfd9b9eb60689647c16d7067d87665e46d
Gaëtan H <steellgold0@gmail.com>
2022-08-21 15:53:09 +0200

    Remove comments

--- src/lib/components/layouts/Navbar.svelte
+++ src/lib/components/layouts/Navbar.svelte
@@ -38,26 +38,6 @@
 <style lang="scss">
   @import "../../scss/variables.scss";
 
-  // .waves {
-  //   position: absolute;
-  //   top: 0;
-  //   left: 0;
-  //   z-index: -1;
-  //   color: aliceblue;
-
-  //   img {
-  //     width: 100%;
-  //     height: 100%;
-  //   }
-
-  //   @media (max-width: 768px) {
-  //     img {
-  //       width: 100%;
-  //       height: 200px;
-  //     }
-  //   }
-  // }
-
   nav {
     padding: 20px;
     display: flex;
