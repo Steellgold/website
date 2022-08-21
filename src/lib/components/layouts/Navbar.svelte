5ec305f09e4f7f110e7b99ed9c78dede10ca82a6
Gaëtan H <steellgold0@gmail.com>
2022-08-21 22:00:28 +0200

    Spotify :heart:

--- src/lib/components/layouts/Navbar.svelte
+++ src/lib/components/layouts/Navbar.svelte
@@ -3,17 +3,36 @@
 
   let online: string = "offline";
   let avatar = "";
+  
+  interface ListeningProp {
+    songName: string;
+    artistName: string;
+    albumName: string;
+    albumArtUrl: string;
+  }
+
+  let listening: ListeningProp | null = null;
 
   onMount(async () => {
     const response = await fetch("https://api.lanyard.rest/v1/users/504392983244832780");
     const data = await response.json();
     online = data.data.discord_status;
     avatar = data.data.discord_user.avatar;
+
+    if (data.data.spotify) {
+      listening = {
+        songName: data.data.spotify.song,
+        artistName: data.data.spotify.artist,
+        albumName: data.data.spotify.album,
+        albumArtUrl: data.data.spotify.album_art_url,
+      }
+    }
+
   });
 </script>
 
 <div>
-  <nav>
+  <nav class="navbar">
     <div class="discord-status">
       <img id="discord-avatar" src="https://cdn.discordapp.com/avatars/504392983244832780/{avatar}.png" alt="discord">
       <span class="back-status"></span>
@@ -30,13 +49,27 @@
     </ul>
   </nav>
 
+  <nav class="rich-presence">
+    {#if listening}
+      <div>
+        <h1>Écoute sur Spotify</h1>
+        <p>Musique: {listening.songName}</p>
+        <p>Album: {listening.albumName}</p>
+        <p>Groupe: {listening.artistName}</p>
+      </div>
+      <div>
+        <img src="{listening.albumArtUrl}" alt="Album Art" height="100px">   
+      </div> 
+    {/if}
+  </nav>
+
   <!-- A faire: Faire une version mobile (😰) -->
 </div>
 
 <style lang="scss">
   @import "../../scss/variables.scss";
 
-  nav {
+  nav.navbar {
     padding: 20px;
     display: flex;
     justify-content: space-between;
@@ -116,6 +149,25 @@
         a:hover:not(.active) {
           background-color: $color-background-hover;
         }
+      }
+    }
+  }
+
+  nav.rich-presence {
+    background-color: $color-background-100-opacity;
+    display: flex;
+    padding: 10px;
+    margin: 0;
+    justify-content: space-between;
+    align-items: center;
+    
+    img {
+      border-radius: 20px;
+      border: 3px solid #FFFFFF;
+      
+      @media (max-width: 768px) {
+        justify-content: space-between;
+        align-items: center;
       }
     }
   }
