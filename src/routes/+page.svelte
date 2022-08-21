3b1e76db9cbdd9a88b699d35c34b3878a790737a
Gaëtan H <steellgold0@gmail.com>
2022-08-21 14:11:17 +0200

    Description of "my" projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,11 +8,11 @@
 
 <header>
   <div class="title">
-    <h1>Salut</h1>
+    <h1>Hey</h1>
     <img class="wave" src = "/images/FluentWave.svg" alt="Wave"/>
   </div>
 
-  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis est, officiis aliquid voluptates quidem esse suscipit consequuntur repellat cupiditate fugit recusandae hic voluptatibus molestias adipisci natus! Cupiditate reprehenderit non aspernatur!</p>
+  <p>Gaëtan, 17 years old, passionate about programming and computer science, I am a PHP developer, I am currently learning Web development and Java<p>
 
   <div class="buttons">
     <Button link="https://github.com/Steellgold" icon="dark/GitHub.png">GitHub</Button>
@@ -24,20 +24,20 @@
 <section class="projects">
   <div class="list">
     <Project title="Portfolio" icon="svelte" github_link="https://github.com/Steellgold/Portfolio" techs={["typescript","javascript","html", "css", "sass"]}>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi quae aliquid qui ex laboriosam minus, assumenda est maxime nam et pariatur accusantium eum. Eligendi, unde assumenda! Esse quae voluptate omnis.
+      I decided to make a Portfolio myself using a web framework "Svelte" where I know absolutely nothing about the basics, nor in CSS or I do it myself without Bootstrap or others.<br>I'm not a web developer at all and I really enjoy using Svelte since I started.
     </Project>
     <Project title="Lunastia" icon="java" year="2022 → now" techs={["spigot","maven"]}>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi quae aliquid qui ex laboriosam minus, assumenda est maxime nam et pariatur accusantium eum. Eligendi, unde assumenda! Esse quae voluptate omnis.
+      Minecraft server on the Desktop version, written in <b>Java</b> on the <b>Spigot</b> software, with the primary goal of training, learning Java, and secondarily opening a server on a version with a language that is not my <abbr title="I'm used to PHP with PocketMine-MP">comfort zone</abbr>
     </Project>
     <Project title="RoyaumeBot" icon="typescript" github_link="https://github.com/Virtual-Royaume/Royaume-Discord-Bot" year="2022" techs={["nodejs","discordjs"]} participe=true>
       It is the bot of our Discord community, it offers various utility features. It is written in TypeScript and uses the DiscordJS package to interact with the Discord API.
     </Project>
     <Project title="Rox" icon="typescript" year="2020 → now" co_author="RomainSav" techs={["nodejs", "discordjs"]}>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi quae aliquid qui ex laboriosam minus, assumenda est maxime nam et pariatur accusantium eum. Eligendi, unde assumenda! Esse quae voluptate omnis.
+      A bot initially developed by me alone in JavaScript without having concrete knowledge, it reached <b><abbr title="Total number of members from the servers where they were">100 000 users</abbr></b> but I stopped the development due to lack of knowledge and lack of time <br>Resumed in 2022 in TypeScript with RomainSav to relaunch it, with a clean, uncluttered and understandable code.
     </Project>
     <Project title="Atlas" icon="php" github_link="https://github.com/Steellgold/AtlasCore" year="2019 → 2020" techs={["pmmp"]}>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi quae aliquid qui ex laboriosam minus, assumenda est maxime nam et pariatur accusantium eum. Eligendi, unde assumenda! Esse quae voluptate omnis.
-    </Project>
+      A server with <b>4100+ unique players</b> (at the time of my departure). I developed the whole first version as well as the pre-release which was under another name (RivalityShock PvP) which lasted 4 months (from the moment I arrived), this is where I was able to use my knowledge of PocketMine-MP and get to where I am now
+    </Project> 
   </div>
 </section>
 
