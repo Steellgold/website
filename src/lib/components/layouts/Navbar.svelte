f172e069133d1507eeb84b9cd6cf7969ca933b39
Gaëtan H <steellgold0@gmail.com>
2022-08-23 02:28:06 +0200

    test for env (n*2)

--- src/lib/components/layouts/Navbar.svelte
+++ src/lib/components/layouts/Navbar.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+import { env } from "$env/dynamic/private";
+
+
   export let data: any; 
 </script>
 
 <div>
-  {#if process.env.IS_PREVIEW == "false"}
-    <div class="preview">
-      <p>This is a development mode of the portfolio, you may run into a bug or something like that, go to the <a href="https://steellgold.fr">stable version</a> to be sure not to be surprised</p>
-    </div>
-  {/if}
+  <div class="preview">
+    <p>This is a development mode of the portfolio, you may run into a bug or something like that, go to the <a href="https://steellgold.fr">stable version</a> to be sure not to be surprised</p>
+    <p>{env.IS_PREVIEW}</p>
+  </div>
 
   <nav class="navbar">
     <div class="discord-status">
