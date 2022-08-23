9b41c746a681d0027c6105ea4f55c73ece630b95
Gaëtan H <steellgold0@gmail.com>
2022-08-23 17:50:01 +0200

    Link to commit

--- src/lib/components/layouts/Navbar.svelte
+++ src/lib/components/layouts/Navbar.svelte
@@ -6,7 +6,7 @@
   {#if process.env.IS_PREVIEW}
     <div class="preview">
       <p>This is a development mode of the portfolio, you may run into a bug or something like that, go to the <a href="https://steellgold.fr">stable version</a> to be sure not to be surprised</p>
-      <p>Deployed from commit: {process.env.VERCEL_GIT_COMMIT_MESSAGE}</p>
+      <a href="https://github.com/Steellgold/Portfolio/commit/{process.env.VERCEL_GIT_COMMIT_SHA}">Deployed from commit: {process.env.VERCEL_GIT_COMMIT_MESSAGE}</a>
     </div>
   {/if}
 
