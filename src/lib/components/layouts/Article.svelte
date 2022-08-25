231b053fc0a269bd391f8ba2b49067737d7f47e1
Gaëtan H <steellgold0@gmail.com>
2022-08-25 14:05:17 +0200

    Stylize (wow) the article card

--- src/lib/components/layouts/Article.svelte
+++ src/lib/components/layouts/Article.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
+  export let id: string;
   export let title: string;
   export let published_date: string;
   export let small_description: string;
   export let primaryTechIcon: string;
+  export let banner: string | null = null;
 </script>
 
 <div class="article">
-  <img class="icon" src="/icons/techs/{primaryTechIcon}.png" alt="primary skill"/>
+  {#if banner}
+    <img class="banner" src="{banner}" alt="Banner">
+  {/if}
+
   <div class="infos">
-    <h2>{title}</h2>
-    <p class="subtitle">{small_description}</p>
+    <img class="icon" src="/icons/techs/{primaryTechIcon}.png" alt="primary skill"/>
+
+    <div>
+      <h2>{title}</h2>
+      <p class="subtitle">{small_description}</p>
+      <p class="date">Published at {published_date}</p>
+    </div>
   </div>
 </div>
 
@@ -17,18 +27,28 @@
   @import "../../scss/variables.scss";
 
   .article {
-    display: flex;
     background-color: $color-primary;
     border-radius: 20px;
     width: 600px;
     padding: 10px;
 
+    .banner {
+      border-radius: 26px;
+      width: 100%;
+      height: 200px;
+    }
+
     .infos {
+      padding: 5px 0px;
+      gap: 4px;
       display: flex;
-      flex-direction: column;
-      justify-content: center;
       width: 100%;
       
+      img.icon {
+        margin-right: 1rem;
+        border-radius: 20px;
+      }
+
       h2 {
         font-weight: 800;
         color: white;
@@ -38,7 +58,15 @@
       p.subtitle {
         color: $color-gray;
         font-weight: 500;
-        font-size: 20px;
+        font-size: 15px;
+      }
+
+      p.date {
+        color: $color-gray;
+        font-weight: 500;
+        font-size: 10px;
+        display: flex;
+        justify-content: flex-end;
       }
 
       @media screen and (max-width: 768px) {
@@ -56,11 +84,6 @@
 
     @media screen and (max-width: 768px) {
       width: 100%;
-    }
-
-    img.icon {
-      margin-right: 1rem;
-      border-radius: 20px;
     }
   }
 </style>
