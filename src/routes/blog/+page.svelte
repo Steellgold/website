210e406154b9121d84c766637d198695359b7f07
Gaëtan H <steellgold0@gmail.com>
2022-08-25 23:12:37 +0200

    Update +page.svelte

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,24 +4,27 @@
   import { collection, getFirestore, onSnapshot } from "firebase/firestore";
   import { firebaseConfig } from "$lib/config/firebase";
   import { browser } from "$app/env";
-
+  
   const firebaseApp = browser && (getApps().length === 0 ? initializeApp(firebaseConfig) : getApp());
   const db = browser && getFirestore();
   const colRef = browser && collection(db, "articles");
 
   interface Article {
+    id: string;
     title: string;
-    primary_tech: string;
     published_date: string;
     small_description: string;
+    primary_tech: string;
     banner_url: string | null;
+    tags: string[] = [];
+    difficulty: string;
   }
 
   let articles: Article[] = [];
   const unsubscribe = browser &&
-    onSnapshot(colRef, (querySnapshot) => {
+    onSnapshot(colRef, (querySnapshot: any) => {
       let content: Article[] = [];
-      querySnapshot.forEach((doc) => {
+      querySnapshot.forEach((doc: any) => {
         let article = {...doc.data(), id: doc.id};
         content.push(article);
       })
