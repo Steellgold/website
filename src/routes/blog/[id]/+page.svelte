ed6407bb99738950c37f14bd42dc0ea779a93bb6
Gaëtan H <steellgold0@gmail.com>
2022-08-25 23:12:39 +0200

    Create +page.svelte

---
+++ src/routes/blog/[id]/+page.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import Article from "$lib/components/layouts/Article.svelte";
+
+  /** @type {import('./$types').PageLoad} */
+  export let data;
+
+  console.log(data);
+</script>
+
+<div class="article">
+  <img class="banner" src="https://media.discordapp.net/attachments/951208784200630392/1012395263828181062/Rectangle_123_1.png" alt="">
+
+  <div class="content">
+    
+  </div>
+</div>
+
+<style lang="scss">
+  @import "../../../lib/scss/variables.scss";
+  
+  img.banner {
+    height: 50%;
+    border-radius: 0px 0px 40px 40px;
+
+    @media (max-width: 768px) {
+      width: 90%;
+      border-radius: 0px 0px 20px 20px;
+    }
+  }
+
+  .article {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    p {
+      color: $color-gray;
+      width: 50%;
+      padding: 5px;
+      margin: auto;
+    }
+
+    img.article-img {
+      margin: auto;
+      padding: 15px;
+      display: block;
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+      color: #fff;
+      padding: 10px;
+      margin: auto;
+      text-align: center;
+      text-transform: uppercase;
+    }
+
+    a {
+      color: #fff;
+    }
+
+    ul {
+      list-style-type: none;
+      padding: 0;
+      margin: 0;
+      margin: auto;
+      text-align: center;
+    }
+
+    @media (max-width: 768px) {
+      p {
+        width: 90%;
+      }
+    }
+  }
+</style>
