5b5aef323e993b80890e4e7b443986547867b805
Gaëtan H <steellgold0@gmail.com>
2022-08-27 22:59:46 +0200

    add

---
+++ src/lib/components/elements/form/Input.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let label: string | null = null;
+  export let value: string | null = null;
+  export let type: "text" | "number" | "email" | "tel" | "url" | "password";
+  export let placeholder: string | null = null;
+  export let bind;
+</script>
+
+<input type={type} placeholder={placeholder} value={value}>
+
+<style lang="scss">
+  input {
+    padding: 13px;
+    width: 20%;
+
+    background-color: #1E2A42;
+    border: 3px solid #1E2A42;
+    border-radius: 13px;
+    color: #A9A9A9;
+    font-weight: 600;
+
+    &:focus {
+      outline: none;
+      border: 3px solid #37435E;
+    }
+
+    &::placeholder {
+      text-transform: uppercase;
+    }
+
+    @media (max-width: 768px) {
+      width: 50%;
+    }
+
+    @media (max-width: 820px) {
+      width: 40%;
+    }
+  }
+</style>
---
+++ src/lib/components/elements/form/Option.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  export let options: {
+    value: string | number | boolean;
+    label: string;
+  }[];
+
+  let i = 0;
+</script>
+
+<select>
+  {#each options as option}
+    {#if i == 0}
+      <option value={option.value} selected hidden disabled>{option.label}</option>
+    {:else}
+      <option value={option.value}>{option.label}</option>
+    {/if}
+    {i++}
+  {/each}
+</select>
+
+<style lang="scss">
+  select {
+    padding: 13px;
+    width: 20%;
+    background-color: #1E2A42;
+    border: 3px solid #1E2A42;
+    border-radius: 13px;
+    color: #A9A9A9;
+    font-weight: 600;
+    &:focus {
+      outline: none;
+      border: 3px solid #37435E;
+    }
+    &::placeholder {
+      text-transform: uppercase;
+    }
+    @media (max-width: 768px) {
+      width: 50%;
+    }
+    @media (max-width: 820px) {
+      width: 40%;
+    }
+  }
+</style>
