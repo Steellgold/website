341fe08a139947f66b8aa91d03468062e5d36afe
Gaëtan H <steellgold0@gmail.com>
2022-08-28 19:22:18 +0200

    clean

--- src/lib/components/elements/form/Input.svelte
+++ src/lib/components/elements/form/Input.svelte
@@ -1,34 +1,27 @@
 <script lang="ts">
   export let value: string | null = null;
-  export let type: "text" | "number" | "email" | "tel" | "url" | "password" = "tewt";
+  export let type: "text" | "number" | "email" | "tel" | "url" | "password" = "text";
   export let placeholder: string | null = null;
 </script>
 
 <input type={type} placeholder={placeholder} value={value ?? ""}>
 
 <style lang="scss">
+  @import '../../../scss/variables.scss';
+
   input {
     padding: 13px;
-    width: 20%;
 
-    background-color: #1E2A42;
-    border: 3px solid #1E2A42;
-    border-radius: 13px;
-    color: #A9A9A9;
-    font-weight: 600;
-    font-family: 'Inter';
+    background-color: $color-background-input;
+    border: 3px solid $color-background-input;
+    border-radius: $input-border-radius;
+    color: $color-background-input-text;
+    font-weight: $font-weight-600;
+    font-family: $font-family;
 
     &:focus {
       outline: none;
-      border: 3px solid #37435E;
-    }
-
-    @media (max-width: 768px) {
-      width: 50%;
-    }
-
-    @media (max-width: 820px) {
-      width: 40%;
+      border: 3px solid $color-background-input-focus;
     }
   }
 </style>
