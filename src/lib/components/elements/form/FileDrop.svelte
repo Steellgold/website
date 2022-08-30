476ca6efccb7deb65a5eb4d12b857b92fff6c232
Gaëtan H <steellgold0@gmail.com>
2022-08-30 11:54:52 +0200

    some things

--- src/lib/components/elements/form/FileDrop.svelte
+++ src/lib/components/elements/form/FileDrop.svelte
@@ -2,14 +2,23 @@
   export let isMultiple: boolean = false;
   export let label: string = "Choose an image for the article banner, by clicking or dragging a file here";
 
-  if (!isMultiple) {
-    
+  function onChange(e: any) {
+    console.log(e);
+    // get the file
+    const file = e.target.files[0];
+    console.log(file);
+    // check if it's an image
+    if (!file.type.startsWith("image/")) {
+      document.getElementById("error").innerHTML = "Please upload an image file";
+      return;
+    }
   }
 </script>
 
 <div class="file-drop-area">
   <span class="file-msg">{label}</span>
-  <input class="file-input" type="file" multiple={isMultiple} />
+  <span id="error" class="error-msg"></span>
+  <input class="file-input" type="file" multiple={isMultiple} on:change="{onChange}" />
 </div>
 
 <style lang="scss">
