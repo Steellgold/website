81c370b4fd4b88ddcde6b933a51c06815a45d6ca
Gaëtan H <steellgold0@gmail.com>
2022-08-31 22:29:11 +0200

    change somethings

--- src/lib/components/elements/form/select/select.story.md
+++ src/lib/components/elements/form/select/select.story.md
@@ -1,1 +1,76 @@
 # Select
+
+```svelte
+<Select options={
+  [
+    {
+      value: "css",
+      label: "CSS"
+    },
+    {
+      value: "discord",
+      label: "Discord"
+    },
+    {
+      value: "discordjs",
+      label: "DiscordJS"
+    },
+    {
+      value: "figma",
+      label: "Figma"
+    },
+    {
+      value: "firebase",
+      label: "Firebase"
+    },
+    {
+      value: "html",
+      label: "HTML"
+    },
+    {
+      value: "java",
+      label: "Java"
+    },
+    {
+      value: "javascript",
+      label: "JavaScript"
+    },
+    {
+      value: "maven",
+      label: "Maven"
+    },
+    {
+      value: "nodejs",
+      label: "NodeJS"
+    },
+    {
+      value: "php",
+      label: "PHP"
+    },
+    {
+      value: "pmmp",
+      label: "PocketMine-MP"
+    },
+    {
+      value: "sass",
+      label: "SCSS"
+    },
+    {
+      value: "spigot",
+      label: "Spigot"
+    },
+    {
+      value: "svelte",
+      label: "Svelte"
+    },
+    {
+      value: "symfony",
+      label: "Symfony"
+    },
+    {
+      value: "typescript",
+      label: "TypeScript"
+    }
+  ]
+} />
+```
--- src/lib/components/elements/form/select/select.story.svelte
+++ src/lib/components/elements/form/select/select.story.svelte
@@ -5,5 +5,76 @@
 </script>
 
 <Hst.Story title="Select">
-  <p>Select</p>
+  <Select options={
+    [
+      {
+        value: "css",
+        label: "CSS"
+      },
+      {
+        value: "discord",
+        label: "Discord"
+      },
+      {
+        value: "discordjs",
+        label: "DiscordJS"
+      },
+      {
+        value: "figma",
+        label: "Figma"
+      },
+      {
+        value: "firebase",
+        label: "Firebase"
+      },
+      {
+        value: "html",
+        label: "HTML"
+      },
+      {
+        value: "java",
+        label: "Java"
+      },
+      {
+        value: "javascript",
+        label: "JavaScript"
+      },
+      {
+        value: "maven",
+        label: "Maven"
+      },
+      {
+        value: "nodejs",
+        label: "NodeJS"
+      },
+      {
+        value: "php",
+        label: "PHP"
+      },
+      {
+        value: "pmmp",
+        label: "PocketMine-MP"
+      },
+      {
+        value: "sass",
+        label: "SCSS"
+      },
+      {
+        value: "spigot",
+        label: "Spigot"
+      },
+      {
+        value: "svelte",
+        label: "Svelte"
+      },
+      {
+        value: "symfony",
+        label: "Symfony"
+      },
+      {
+        value: "typescript",
+        label: "TypeScript"
+      }
+    ]
+  } />
 </Hst.Story>
