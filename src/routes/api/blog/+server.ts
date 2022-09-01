0759898f00f82d3a289e080c6990197f009df5e3
Gaëtan H <steellgold0@gmail.com>
2022-09-01 11:55:03 +0200

    introduce GET api call

--- src/routes/api/blog/+server.ts
+++ src/routes/api/blog/+server.ts
@@ -1,6 +1,6 @@
-import type { RequestEvent } from '@sveltejs/kit';
-import { collection, addDoc } from 'firebase/firestore';
 import { db } from '$lib/database/Firebase';
+import type { RequestEvent } from '@sveltejs/kit';
+import { collection, addDoc, doc, getDoc } from 'firebase/firestore';
 import { z } from 'zod';
 import dayjs from 'dayjs';
 
@@ -18,7 +18,6 @@ export async function POST({ request }: RequestEvent): Promise<Response> {
   });
 
   const bodyParsed = bodySchema.safeParse(values);
-  console.log(values, bodyParsed);
 
   if (!bodyParsed.success) {
     return new Response(bodyParsed.error.message, { status: 400 });
@@ -34,4 +33,20 @@ export async function POST({ request }: RequestEvent): Promise<Response> {
   })
 
   return new Response('Article was been successfuly created', { status: 200 });
+}
+
+export async function GET({ request }: RequestEvent): Promise<Response> {
+  const values = await request.json();
+  const bodySchema = z.object({
+    id: z.string(),
+  });
+
+  const docRef = doc(db, "articles", values.id);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    return new Response(JSON.stringify(docSnap.data()), { status: 200 });
+  } else {
+    return new Response('Article not found', { status: 40 });
+  }
 }
