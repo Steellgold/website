aab1595034110ff3f60988bb29eb02f01dea6482
Gaëtan H <steellgold0@gmail.com>
2022-09-03 00:39:05 +0200

    sexy navbar for mobile
    Co-Authored-By: LMGerard <52181828+LMGerard@users.noreply.github.com>

--- src/lib/components/layouts/navbar/navbar.svelte
+++ src/lib/components/layouts/navbar/navbar.svelte
@@ -1,23 +1,20 @@
 <script lang="ts">
-
+  let active = false;
 </script>
 
 <div class="nav">
-  <img src="/profile.jpg" alt="Logo">
+  <div class="logo">
+    <img src="/profile.jpg" alt="Logo">
+  </div>
 
-  <div class="links">
+  <div class="links" class:active={active}>
     <a href="/">Réalisations</a>
     <a href="/">Projets</a>
     <a href="/">Blog</a>
   </div>
 
-  <i class="fa-solid fa-bars"></i>
+  <i class="fa-solid " class:fa-x={!active} class:fa-bars={active} on:click={() => active = !active}></i>
 </div>
-
-<br>
-<br>
-<br>
-<br>
 
 <style lang="scss">
   @import "../../../scss/colors.scss";
@@ -27,24 +24,51 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
+    background: $primary-gradiant;
 
     height: 7vh;
     padding: 0 25px;
-    background: $primary-gradiant;
+
+    i {
+      display: none;
+    }
 
     @media (max-width: $breakpoint-md) {
       flex-direction: column;
+      background: none;
       height: fit-content;
-      gap: 6px;
-      padding: 10px 0;
+      padding: 0 0;
+      position: relative;
+
+      i {
+        position: absolute;
+        display: block;
+        font-size: 30px;
+
+        top: 30px;
+        right: 30px;
+        z-index: 20;
+      }
+
+      .logo {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        background: $primary-gradiant;
+        z-index: 10;
+      }
     }
 
-    img {
-      border-radius: 10%;
-      height: 80%;
+    .logo {
+      padding: 10px 0;
+      
+      img {
+        border-radius: 10%;
+        height: 80%;
 
-      @media (max-width: $breakpoint-md) {
-        height: 70px;
+        @media (max-width: $breakpoint-md) {
+          height: 70px;
+        }
       }
     }
 
@@ -59,7 +83,24 @@
       }
 
       @media (max-width: $breakpoint-md) {
+        position: absolute;
+        top: 100%;
+
         flex-direction: column;
+        transition-duration: 0.50s;
+
+        background: $primary-gradiant;
+        width: 100%;
+
+        padding-top: 10px;
+        padding: 10px 0;
+        gap: 3px;
+        height: fit-content;
+
+        &.active {
+          // transform: translateY(-100%);
+          top: 0;
+        }
       }
     }
   }
