225a98d0bf825661f68d3519a2af9be8e37702e7
Gaëtan H <steellgold0@gmail.com>
2022-09-02 23:31:38 +0200

    histoire

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
+import { HstSvelte } from '@histoire/plugin-svelte';
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { defineConfig } from 'histoire';
+import { fileURLToPath, URL } from 'url';
 
-const config: UserConfig = {
-	plugins: [sveltekit()]
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit(), defineConfig],
+	histoire: {
+		plugins: [HstSvelte()],
+		vite: {
+			resolve: {
+				alias: {
+					$lib: fileURLToPath(new URL('./src/lib', import.meta.url)),
+					$app: fileURLToPath(new URL('./histoire/app', import.meta.url)),
+				},
+			},
+		},
+	}
 };
 
 export default config;
