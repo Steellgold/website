328e7003fcbc418af1ff086b9bf1548add44d3fb
Gaëtan H <steellgold0@gmail.com>
2022-09-03 23:35:12 +0200

    some docs

--- src/lib/components/elements/text/text.story.svelte
+++ src/lib/components/elements/text/text.story.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
+  import { Text } from ".";
+  export let Hst: any;
+
+  export let text = "Lorem ipsum dolor sit amet et velit deleniti et porro doloribus et nobis deleniti.";
+  export let type: "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" = "p";
+  export let color: "white" | "primary" | "secondary" | "tertiary" | "default" = "default";
+  export let align: "left" | "center" | "right" | "default" = "default";
+  export let italic: boolean = false;
+  export let textTransform: "none" | "capitalize" | "uppercase" | "lowercase" = "none";
 </script>
-<Hst.Story title="components/elements/Text">
+
 <Hst.Story title="components/elements/Text" icon="ic:outline-clear-all">
   <svelte:fragment slot="controls">
+    <Hst.Text bind:value={text} title="Text" />
+    <Hst.Select bind:value={type} title="aa" options={["p", "h1", "h2", "h3", "h4", "h5", "h6"]} />
+    <Hst.Select bind:value={color} title="Color" options={["white", "primary", "secondary", "tertiary", "default"]} />
+    <Hst.Select bind:value={align} title="Align" options={["left", "center", "right", "default"]} />
+    <Hst.Checkbox bind:value={italic} title="Italic" />
+    <Hst.Select bind:value={textTransform} title="Text Transform" options={["none", "capitalize", "uppercase", "lowercase"]} />
   </svelte:fragment>
+
+  <Text type={type} color={color} align={align} italic={italic} textTransform={textTransform}>
+    {text}
+  </Text>
 </Hst.Story>
---
+++ src/lib/components/elements/text/text.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+  export let type: "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" = "p";
+  export let color: "white" | "primary" | "secondary" | "tertiary" | "default" = "default";
+  export let align: "left" | "center" | "right" | "default" = "default";
+  export let italic: boolean = false;
+  export let textTransform: "none" | "capitalize" | "uppercase" | "lowercase" = "none";
+  
+  let classes = "";
+  
+  $: classes = `text-${type} text-color-${color} text-align-${align} ${italic ? "text-italic" : ""} text-transform-${textTransform}`;
+</script>
+
+<p class={classes}><slot /></p>
+
+<style lang="scss">
+  @import "../../../scss/colors.scss";
+  @import "../../../scss/variables.scss";
+
+  .text-p {
+    font-size: 16px;
+    font-weight: 400;
+  }
+
+  .text-h1 {
+    font-size: 32px;
+    font-weight: 700;
+  }
+
+  .text-h2 {
+    font-size: 24px;
+    font-weight: 700;
+  }
+
+  .text-h3 {
+    font-size: 18px;
+    font-weight: 700;
+  }
+
+  .text-h4 {
+    font-size: 16px;
+    font-weight: 700;
+  }
+
+  .text-h5 {
+    font-size: 13px;
+    font-weight: 700;
+  }
+
+  .text-h6 {
+    font-size: 10px;
+    font-weight: 700;
+  }
+
+  .text-color-white {
+    color: $white;
+  }
+
+  .text-color-primary {
+    color: $primary;
+  }
+
+  .text-color-secondary {
+    color: $secondary;
+  }
+
+  .text-color-tertiary {
+    color: $tertiary;
+  }
+
+  .text-italic {
+    font-style: italic;
+  }
+
+  .text-align-left {
+    text-align: left;
+  }
+
+  .text-align-center {
+    text-align: center;
+  }
+
+  .text-align-right {
+    text-align: right;
+  }
+
+  .text-transform-capitalize {
+    text-transform: capitalize;
+  }
+
+  .text-transform-uppercase {
+    text-transform: uppercase;
+  }
+
+  .text-transform-lowercase {
+    text-transform: lowercase;
+  }
+</style>
