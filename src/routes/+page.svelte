61f7c55bbf429572b01f1b07709cdb9b57aeb881
Gaëtan H <steellgold0@gmail.com>
2022-09-04 21:09:56 +0200

    some

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
   import { Text } from "$lib/components/elements/text";
+  import { Button } from "$lib/components/elements/button";
 </script>
 
 <header>
   <div class="title">
-    <Text type="h1" size="3xl" weight="bold" align="center">Hey 👋</Text>
+    <Text type="h1" size="4xl" weight="bold" align="center">Hey 👋</Text>
   </div>
 
   <Text align="center" width={50}>
     Gaëtan, 17 years old, passionate about programming and computer science, I am a PHP developer, I am currently learning Web development and Java
   </Text>
+
+  <div class="buttons">
+    <Button link="https://steellgold.fr" iconType="fontawesome" icon="fa-brands fa-github" iconSize="md"  disabled={false}>
+      Gaëtan 🐈#2728
+    </Button>
 </header>
 
 <style lang="scss">
@@ -17,13 +23,15 @@
   @import "../lib/scss/variables.scss";
 
   header {
+    padding: 20px;
+
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
 
     .title {
-      margin-bottom: 1rem;
+      margin-bottom: 10px;
     }
   }
 </style>
