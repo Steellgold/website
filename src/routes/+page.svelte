9fbf41cb596bf5f5339de75dcc71ea23aa183f3b
Gaëtan H <steellgold0@gmail.com>
2022-09-04 11:43:47 +0200

    some changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,19 @@
   import { Text } from "$lib/components/elements/text";
 </script>
 
-<Text type="h1">Hey</Text>
+<header>
+  <Text type="h1" size="3xl" weight="bold" align="center">Hey 👋</Text>
+  <Text type="p" align="center" width={50}>
+    Gaëtan, 17 years old, passionate about programming and computer science, I am a PHP developer, I am currently learning Web development and Java
+  </Text>
+</header>
 
 <style lang="scss">
-    
+  header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    padding: 2rem 0;
+  }
 </style>
