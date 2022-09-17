b3a19876ab5707dfaf5f0fb69e0ea30baef126de
Gaëtan H <steellgold0@gmail.com>
2022-09-17 17:41:54 +0200

    change

--- src/lib/components/layouts/navbar/navbar.story.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import Navbar from "./navbar.svelte";
-
-  export let Hst: any;
-</script>
-
-<Hst.Story title="components/layouts/Navbar" icon="ic:baseline-border-top">
-  <Navbar />
-</Hst.Story>
