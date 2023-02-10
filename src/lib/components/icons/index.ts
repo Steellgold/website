08b5c9b4387c3a887ca2a13e1b01bfec59e9bc06
Gaëtan H <steellgold0@gmail.com>
2023-02-11 00:38:25 +0100

    🪡 Icons

--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as IconBrandTwitter } from './brand/brand-twitter.svelte';
+export { default as IconBrandInstagram } from './brand/brand-instagram.svelte';
 export { default as IconBrandDiscord } from './brand/brand-discord.svelte';
 export { default as IconBrandYoutube } from './brand/brand-youtube.svelte';
 export { default as IconBrandGithub } from './brand/brand-github.svelte';
--- src/lib/sections/Header.svelte
+++ src/lib/sections/Header.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { IconBrandDiscord, IconBrandGithub, IconBrandTwitter, IconBrandYoutube } from "$lib/components/icons";
+  import { IconBrandDiscord, IconBrandGithub, IconBrandInstagram, IconBrandLinkedin, IconBrandTwitter, IconBrandYoutube } from "$lib/components/icons";
 </script>
 
 <section>
@@ -13,7 +13,9 @@
         <a href="https://github.com/Steellgold" class="block hover:text-white transition-all"><IconBrandGithub /></a>
         <a href="https://discord.com/users/504392983244832780" class="block hover:text-gray-600 transition-all"><IconBrandDiscord /></a>
         <a href="https://youtube.com/c/Steellgold" class="block hover:text-red-600 transition-all"><IconBrandYoutube /></a>
+        <a href="https://instagram.com/steellgold" class="block hover:text-pink-600 transition-all"><IconBrandInstagram /></a>
         <a href="https://twitter.com/Steellgold" class="block hover:text-blue-600 transition-all"><IconBrandTwitter /></a>
+        <a href="https://linkedin.com/gaetanhus" class="block hover:text-blue-600 transition-all"><IconBrandLinkedin /></a>
       </ul>
     </div>
   </div>
