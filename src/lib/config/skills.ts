2a8fc7dc84e2cbf67b081d69f9ed5588a49f8fec
Gaëtan H <steellgold0@gmail.com>
2023-02-11 00:38:16 +0100

    📇 Skills

---
+++ src/lib/config/skills.ts
@@ -0,0 +1,19 @@
+import type { Skill } from "./skills.types";
+
+export const Typescript: Skill = { name: "TypeScript", icon: "typescript", color: "#3178C6" };
+export const Javascript: Skill = { name: "JavaScript", icon: "javascript", color: "#F7DF1E" };
+export const Html: Skill = { name: "HTML", icon: "html", color: "#E34F26" };
+export const Css: Skill = { name: "CSS", icon: "css", color: "#1572B6" };
+export const Sass: Skill = { name: "SASS", icon: "sass", color: "#CC6699" };
+export const Tailwindcss: Skill = { name: "TailwindCSS", icon: "tailwindcss", color: "#38B2AC" };
+export const Svelte: Skill = { name: "Svelte", icon: "svelte", color: "#FF3E00" };
+export const Php: Skill = { name: "PHP", icon: "php", color: "#777BB4" };
+export const Vercel: Skill = { name: "Vercel", icon: "vercel", color: "#000000" };
+export const Railway: Skill = { name: "Railway", icon: "railway", color: "#000000" };
+export const Supabase: Skill = { name: "Supabase", icon: "supabase", color: "#49d633" };
+export const Postgresql: Skill = { name: "PostgreSQL", icon: "postgresql", color: "#336791" };
+export const Mysql: Skill = { name: "MySQL", icon: "mysql", color: "#4479A1" };
+export const Firebase: Skill = { name: "Firebase", icon: "firebase", color: "#FFCA28" };
+export const Nodejs: Skill = { name: "NodeJS", icon: "nodejs", color: "#339933" };
+export const Fastify: Skill = { name: "Fastify", icon: "fastify", color: "#503E33" };
+export const Java: Skill = { name: "Java", icon: "java", color: "#007396" };
