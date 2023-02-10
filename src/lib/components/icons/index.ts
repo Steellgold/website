e18b439d07e89dfb81344016d927d32961aeb1a8
Gaëtan H <steellgold0@gmail.com>
2023-02-10 22:05:26 +0100

    🧷 Icons from @tabler/tabler-icons

---
+++ src/lib/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as IconBrandTwitter } from './brand/brand-twitter.svelte';
+export { default as IconBrandDiscord } from './brand/brand-discord.svelte';
+export { default as IconBrandYoutube } from './brand/brand-youtube.svelte';
+export { default as IconBrandGithub } from './brand/brand-github.svelte';
+export { default as IconBrandLinkedin } from './brand/brand-linkedin.svelte';
