5e15788cbc5e9c1e5a0ec33fd4eaa921c29bd321
Gaëtan H <steellgold0@gmail.com>
2023-04-10 15:31:45 +0200

    eslint

--- src/routes/user/+page.server.ts
+++ src/routes/user/+page.server.ts
@@ -4,7 +4,7 @@ import type { Actions } from "./$types";
 export const actions: Actions = {
   login: async({ request }) => {
     const body = Object.fromEntries(await request.formData());
- 
+
     if (!body.email) return { status: 401, body: { error: "Email is required" } };
     const { data, error } = await supabase.auth.signInWithOtp({ email: body.email });
 
