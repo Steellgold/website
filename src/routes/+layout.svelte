907846a9c5d71831e710187e26714924b515ac21
Gaëtan H <steellgold0@gmail.com>
2023-04-21 20:36:03 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script>
   import "../app.css";
   
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
   import Navigation from "./sections/Navigation.svelte";
   import Footer from "./sections/Footer.svelte";
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Navigation />
