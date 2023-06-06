20e2d01176326d6a77c3a7eb15f42ddf768897bb
Gaëtan H <steellgold0@gmail.com>
2023-06-06 11:27:40 +0200

    style(image.svelte): improve image border and popover position feat(image.svelte): add hover effect to image border

--- src/lib/components/image/image.svelte
+++ src/lib/components/image/image.svelte
@@ -6,10 +6,10 @@
 </script>
 
 <div class="relative">
-  <img id="targetImage" class="w-8 h-8 border-2 rounded-full border-gray-800" src={targetImage} alt="icon" on:mouseenter={() => showPopover = true} on:mouseleave={() => showPopover = false} />
+  <img id="targetImage" class="w-8 h-8 border-2 rounded-full border-gray-800 hover:border-gray-300" src={targetImage} alt="icon" on:mouseenter={() => showPopover = true} on:mouseleave={() => showPopover = false} />
   {#if showPopover}
-  <!-- in top of the image -->
-    <div class="popover absolute bg-gray-800 text-white p-2 rounded-lg shadow-lg top-[-35px]">
+    <!-- This is the popover allow the user to see the name of the skill -->
+    <div class="popover absolute bg-gray-800 text-white p-2 rounded-lg shadow-lg top-[-40px]">
       <p>{text}</p>
     </div>
   {/if}
