fe63211954a97b2fb2d3b6e751b3fbe2cce40e20
Gaëtan H <gaetanhus@gmail.com>
2024-04-22 21:07:27 +0200

    Add Birthday component and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { ReactElement } from "react";
 import { Header } from "./lib/header";
 import { Projects } from "./lib/projects";
+import { Birthday } from "@/lib/components/birthday";
 
 const Home = (): ReactElement => {
   return (
     <>
       <Header />
+      <Birthday />
 
-      <div className="justify-center mx-auto w-5/6 lg:w-2/4">
+      <div className="justify-center mx-auto w-5/6 lg:w-2/4 mt-3">
         <Projects />
       </div>
     </>
