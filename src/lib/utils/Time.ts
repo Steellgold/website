efaa222dfd114900591858d6aee46b7755480f48
Gaëtan H <gaetanhus@gmail.com>
2024-04-22 17:48:36 +0200

    Refactor project files and remove unused code

--- src/lib/utils/Time.ts
+++
@@ -1,7 +0,0 @@
-export function fromStart(start: string): string {
-  return `${start} → aujourd'hui`;
-}
-
-export function fromStartToEnd(start: string, end: string): string {
-  return `${start} → ${end}`;
-}
