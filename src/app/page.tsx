7bf139a2d3eca7f7fa8c0c97555428cf7734caf0
Gaëtan H <gaetanhus@gmail.com>
2024-04-22 22:12:23 +0200

    Update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { ReactElement } from "react";
 import { Header } from "./lib/header";
 import { Projects } from "./lib/projects";
 import { Birthday } from "@/lib/components/birthday";
+import { Blog } from "./lib/blog";
+import { Separator } from "@/lib/components/ui/separator";
 
 const Home = (): ReactElement => {
   return (
@@ -11,6 +13,12 @@ const Home = (): ReactElement => {
 
       <div className="justify-center mx-auto w-5/6 lg:w-2/4 mt-3">
         <Projects />
+
+        <Blog />
+
+        <div className="mt-7 mb-7">
+          <p className="text-white text-center">Made with ❤️ by Gaëtan</p>
+        </div>
       </div>
     </>
   );
