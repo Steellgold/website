587302e4d49c8e1dc27aeadce55f309372ec056c
Gaëtan H <gaetanhus@gmail.com>
2024-04-22 22:12:07 +0200

    Add new UI components: Separator, Alert, AlertTitle, and AlertDescription

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: "dgzmokcshopxlsimyjqx.supabase.co", protocol: "https" }
+    ]
+  }
+};
 
 export default nextConfig;
