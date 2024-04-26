1ad0fcd1b3a6d53fbd1e033234ca433f68dad3c5
Gaëtan H <gaetanhus@gmail.com>
2024-04-26 16:54:05 +0200

    Add constants for various activities in presences.consts.ts

---
+++ src/lib/components/presences.consts.ts
@@ -0,0 +1,14 @@
+import { Activity } from "react-use-lanyard";
+
+export const VS_CODE = "782685898163617802";
+export const FIGMA = "768942376403075073";
+export const NETFLIX = "926541425682829352";
+export const YOUTUBE = "463097721130188830";
+export const STACK_OVERFLOW = "610123745033584651";
+export const GITHUB = "607587875122446359";
+export const AMAZON_PRIME_VIDEO = "705139844883677224";
+export const TWITCH = "802958789555781663";
+export const DISNEY_PLUS = "630236276829716483";
+export const CANAL_PLUS = "844106861711196179";
+
+type ActivityName = typeof VS_CODE | typeof FIGMA | typeof NETFLIX | typeof YOUTUBE | typeof STACK_OVERFLOW | typeof GITHUB | typeof AMAZON_PRIME_VIDEO | typeof TWITCH | typeof DISNEY_PLUS | typeof CANAL_PLUS;
