03a4aed9eb21cf527daa387ea2ac86854adb5a3b
Gaëtan H <gaetanhus@gmail.com>
2024-04-26 17:27:51 +0200

    Update lanyard.tsx to include new allowed activities and activity card color

--- src/lib/components/lanyard.tsx
+++ src/lib/components/lanyard.tsx
@@ -6,35 +6,7 @@ import { Component } from "./utils/component";
 import { Card, CardContent, CardFooter } from "./ui/card";
 import Image from "next/image";
 import { cn } from "../utils";
-
-export const ALLOWED_ACTIVITIES = [
-  "782685898163617802", // Visual Studio Code
-  "768942376403075073", // Figma
-  "926541425682829352", // Netflix
-
-  // Not configured yet
-  // "610123745033584651", // Stack Overflow
-  // "607587875122446359", // GitHub
-  // "705139844883677224", // Amazon Prime Video
-  // "802958789555781663", // Twitch
-  // "463097721130188830", // YouTube
-  // "630236276829716483", // Disney+
-  // "844106861711196179", // Canal+
-
-  // TODO: Change this to enums/consts for call the IDs more easily
-];
-
-const CARD_COLORS_MAP: Record<string, string> = {
-  "782685898163617802": "border-[#2bb0f0] bg-[#2bb0f0]/10", // Visual Studio Code,
-  "768942376403075073": "border-[#a357ff] bg-[#a357ff]/10", // Figma
-  "926541425682829352": "border-[#e8040e] bg-[#e8040e]/10", // Netflix
-};
-
-const IMAGES_CONFIG: Record<string, boolean> = {
-  "782685898163617802": false, // Visual Studio Code
-  "768942376403075073": true, // Figma,
-  "926541425682829352": true, // Netflix
-};
+import { AMAZON_PRIME_VIDEO, DISNEY_PLUS, GITHUB, NETFLIX, STACK_OVERFLOW, X, fixActivityGitHubImageLink, getActivityCardColor, getActivityId, isActivityBigImage, isAllowedActivity } from "./presences.consts";
 
 export const DiscordPresence = (): ReactElement | ReactElement[] => {
   const { loading, status } = useLanyard({ userId: "504392983244832780", socket: true })
@@ -50,7 +22,7 @@ export const DiscordPresence = (): ReactElement | ReactElement[] => {
   if (!filteredActivities || filteredActivities.length === 0) return <></>;
 
   const presences = filteredActivities.map(activity => {
-    if (!ALLOWED_ACTIVITIES.includes(activity.application_id!)) return <></>;
+    if (!isAllowedActivity(activity)) return <></>;
     if (activity.application_id === "782685898163617802" && activity.details === "Idling") return <></>;
 
     return (
@@ -66,25 +38,29 @@ const PresenceCard: Component<{ activity: Activity }> = ({ activity }) => {
     <Card className={cn(
       "h-full border-[2px] mb-3 group",
       "transition-colors duration-300 hover:shadow-lg",
-      CARD_COLORS_MAP[activity.application_id!] ?? "",
+      getActivityCardColor(activity),
     )}>
-      <CardContent className="flex flex-row items-center mt-3 -mb-5 px-3">
+      <CardContent className="flex flex-row items-center mt-3 -mb-5 px-3 relative">
         <div className="relative">
           <Image
-            src={getImage(activity, IMAGES_CONFIG[activity.application_id!] ?? true)}
-            alt="Album cover"
+            src={
+              getActivityId(activity) === GITHUB ? 
+                fixActivityGitHubImageLink(activity) :
+                getImage(activity, isActivityBigImage(activity))
+            }
+            alt="Activity cover"
             width={64}
             height={64}
-            className="rounded-lg object-cover"
+            className="rounded-lg object-cover ml-1"
           />
         </div>
           
-        <div className="flex flex-col justify-center ml-5">
+        <div className="flex flex-col justify-center ml-3">
           <h1 className="text-white text-lg font-bold flex flex-row items-center">
             <h1>{activity.name}</h1>
           </h1>
           
-          {activity.state && <p className="text-white text-xs md:text-md">{activity.details}</p>}
+          {activity.details && <p className="text-white text-xs md:text-md">{activity.details}</p>}
           {activity.state && <p className="text-white text-xs md:text-md">{activity.state}</p>}
         </div>
       </CardContent>
@@ -95,9 +71,16 @@ const PresenceCard: Component<{ activity: Activity }> = ({ activity }) => {
 const getImage = (activity: Activity, large = true) => {
   let image = large ? activity.assets?.large_image : activity.assets?.small_image;
 
-  if (activity.application_id === "926541425682829352") {
-    return `https://cdn.rcd.gg/PreMiD/websites/N/Netflix/assets/1.png`;
-  }
+  if (activity.application_id === getActivityId(NETFLIX))
+      return `https://cdn.rcd.gg/PreMiD/websites/N/Netflix/assets/1.png`;
+  if (activity.application_id === getActivityId(AMAZON_PRIME_VIDEO))
+      return `https://cdn.rcd.gg/PreMiD/websites/P/Prime%20Video/assets/logo.png`;
+  if (activity.application_id === getActivityId(STACK_OVERFLOW))
+      return "https://cdn.rcd.gg/PreMiD/websites/S/Stack%20Overflow/assets/logo.png";
+  if (activity.application_id === getActivityId(X))
+      return "https://upload.wikimedia.org/wikipedia/commons/5/57/X_logo_2023_%28white%29.png" // Because on the cdn.rcd.gg the logo is black (This not racism, just is black on black background...)
+  if (activity.application_id === getActivityId(DISNEY_PLUS))
+      return `https://pbs.twimg.com/profile_images/1772881174630912000/zyJT1Mw6_400x400.jpg`; // Twitter because the now logo is not available in the cdn.rcd.gg yet
   
   if (image?.startsWith("mp:external")) {
     return image?.replace(/mp:external\/([^\/]*)\/(http[s])/g, "$2:/");
