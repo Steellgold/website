dafa9935cc121b024bd44ea00dcf19ccac88c637
Gaëtan H <gaetanhus@gmail.com>
2024-04-27 14:04:00 +0200

    Refactor file paths and imports for Wordle dialog component and add WordleParty category and statistics functions

--- src/app/wordle/page.tsx
+++ src/app/wordle/page.tsx
@@ -1,9 +1,25 @@
-import { WordleDialog } from "@/lib/components/wordle.dialog"
+import { WordleDialog } from "@/app/wordle/dialogs/wordle.dialog"
+import Image from "next/image";
+import { WordleParty } from "./party";
 
 const Wordle = () => {
   return (
     <>
-    <WordleDialog />
+      <WordleDialog />
+
+      <div className="justify-center mx-auto w-full px-4 sm:px-0 sm:w-5/6 lg:w-3/6 mt-5 sm:mt-10 md:mt-20">
+        <Image
+          src="/_static/images/wordle_title.png"
+          alt="Wordle"
+          width={400}
+          height={69.27}
+          className="justify-center mx-auto"
+        />
+
+        <div className="mt-5">
+          <WordleParty />
+        </div>
+      </div>
     </>
   )
 }
