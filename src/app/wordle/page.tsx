47f17f181731791e1d5c8d915709132678c129a6
Gaëtan H <gaetanhus@gmail.com>
2024-04-29 20:28:28 +0200

    Refactor Wordle page component to use WordleParty component

--- src/app/wordle/page.tsx
+++ src/app/wordle/page.tsx
@@ -4,23 +4,7 @@ import { WordleParty } from "./party";
 
 const Wordle = () => {
   return (
-    <>
-      <WordleDialog />
-
-      <div className="justify-center mx-auto w-full px-4 sm:px-0 sm:w-5/6 lg:w-3/6 mt-5 sm:mt-10 md:mt-20">
-        <Image
-          src="/_static/images/wordle_title.png"
-          alt="Wordle"
-          width={400}
-          height={69.27}
-          className="justify-center mx-auto"
-        />
-
-        <div className="mt-5">
-          <WordleParty />
-        </div>
-      </div>
-    </>
+    <WordleParty />
   )
 }
 
