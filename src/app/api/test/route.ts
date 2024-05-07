005cadcd3d808f0dc1ed83cbe44bc66c61ecfa2e
Gaëtan H <gaetanhus@gmail.com>
2024-05-07 22:30:32 +0200

    chore: Update imports and remove unused code in multiple files

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,13 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async({ geo, headers, ip }: NextRequest): Promise<NextResponse> => {
-  const data = await fetch("https://ipapi.co/json/");
+export const GET = async(req: NextRequest): Promise<NextResponse> => {
+  console.log("Request Headers", req);
 
   return NextResponse.json({
-    ip,
-    headers,
-    geo,
-    data: await data.json(),
-    dataButWithFromVercel: (await fetch(`https://ipapi.co/${ip}/json/`)).json(),
+    ip: req.ip || "No IP",
+    test: {
+      0: req.ip,
+      1: req.headers,
+      2: req.geo
+    }
+    // dataButWithFromVercel: (await fetch(`https://ipapi.co/${ip}/json/`)).json(),
   });
 }
