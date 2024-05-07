9b65ed58bafd2818ca4ee855caad9d3624b9797c
Gaëtan H <gaetanhus@gmail.com>
2024-05-07 23:07:08 +0200

    feat: Add request IP headers to test API route

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 export const runtime = "edge"
 
 export async function GET(req: Request) {
+  console.log("Request headers", req.headers);
+
   const data = {
     ok: true,
     ip: req.headers.get("x-real-ip") ?? "127.0.0.1",
