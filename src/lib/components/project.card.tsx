a88c8da1604baabfad71997f6e8abef81a546215
Gaëtan H <gaetanhus@gmail.com>
2024-08-04 13:40:27 +0200

    feat: Update ProjectCard to conditionally display cvDescription based on viewMode

--- src/lib/components/project.card.tsx
+++ src/lib/components/project.card.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import Link from "next/link";
 import { Project } from "../config/types/project.type";
 import { cn } from "../utils";
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import { CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
 import { Component } from "./utils/component";
 import { ExternalLink } from "lucide-react";
 import { PropsWithChildren } from "react";
 import { CustomCard } from "./card";
+import { useViewMode } from "../hooks/mode.store";
 
-export const ProjectCard: Component<Project & { className?: string }> = ({ title, description, stacks, url, duration, type, className }) => {
+export const ProjectCard: Component<Project & { className?: string }> = ({ title, description, cvDescription, stacks, url, duration, type, className }) => {
+  const { viewMode } = useViewMode();
+
   return (
     <SurroundLink href={url} className={cn("cursor-pointer", className)}>
       <CustomCard>
@@ -26,7 +31,8 @@ export const ProjectCard: Component<Project & { className?: string }> = ({ title
             
             {type === "pro" && <span className="text-[#181b20] bg-[#f5f1de] px-2 py-1 rounded-md text-xs">Pro</span>}
           </div>
-          <CardDescription>{description}</CardDescription>
+
+          {viewMode == "cv" && cvDescription ? <CardDescription>{cvDescription}</CardDescription> : <CardDescription>{description}</CardDescription>}
         </CardHeader>
 
         <CardFooter className="p-4">
