0e6ce164a6d69c04bb7a3474848eb583b8387e47
Gaëtan H <gaetanhus@gmail.com>
2024-08-04 13:41:33 +0200

    feat: Update Projects component to include CV mode functionality

--- src/app/lib/projects.tsx
+++ src/app/lib/projects.tsx
@@ -1,21 +1,41 @@
+"use client";
+
 import { ProjectCard } from "@/lib/components/project.card";
 import { projects } from "@/lib/config/projects";
+import { useViewMode } from "@/lib/hooks/mode.store";
 import { cn } from "@/lib/utils";
 import { ReactElement } from "react";
 
 export const Projects = (): ReactElement => {
 
+  const { viewMode } = useViewMode();
+
   const isOdd = projects.length % 2 !== 0;
 
-  return (
-    <div className={cn("grid gap-3", {
-      "md:grid-cols-2": projects.length > 1,
+  return <>
+    {viewMode == "cv" && (
+      <div className="rounded-lg shadow-lg mt-4 mb-1.5">
+        <h2 className="text-white text-xl font-bold">
+          Here are my latest realizations
+        </h2>
+
+        <p className="text-white text-sm">
+          I am a developer and I like to create projects to learn new things. Here are some of my latest and best projects.
+        </p>
+      </div>
+    )}
+
+    <div className={cn("gap-3", {
+      "grid": viewMode == "normal",
+      "md:grid-cols-2": projects.length > 1 && viewMode == "normal",
     })}>
       {projects.map((project, index) => (
-        <ProjectCard {...project} key={project.title} className={cn({
-          "md:col-span-2": isOdd && index === projects.length - 1
-        })} /> 
+        <div key={project.title}>
+          <ProjectCard {...project} className={cn({
+            "md:col-span-2": isOdd && index === projects.length - 1 && viewMode == "normal"
+          })} /> 
+        </div>
       ))}
     </div>
-  );
+  </>;
 }
