9a18e950b63b3e65fcf8412705de2011258fbcfc
Gaëtan H <gaetanhus@gmail.com>
2024-08-04 15:59:25 +0200

    refactor: Rename CVButton component to CVButtons

--- src/lib/components/cvButton.tsx
+++ src/lib/components/cvButton.tsx
@@ -3,12 +3,12 @@
 import { useViewMode } from "../hooks/mode.store";
 import { Button } from "./ui/button";
 
-export const CVButton = () => {
+export const CVButtons = () => {
   const { viewMode, setViewMode } = useViewMode();
 
-  return (
+  return <div className="flex flex-row items-center gap-2" id="cv-buttons">
     <Button onClick={() => setViewMode(viewMode == "normal" ? "cv" : "normal")} variant={"outline"} size={"sm"}>
       {viewMode == "normal" ? "View the CV" : "View the Portfolio"}
     </Button>
-  );
+  </div>;
 };
