a9631677d2582850cc7be2a56b67d9520fa74a2c
Gaëtan H <gaetanhus@gmail.com>
2024-10-14 00:11:19 +0200

    refactor(route.ts): change return type of playingEndpoint function from Response to any to handle different response types fix(route.ts): add error handling for failed fetch request in GET function to throw an error and return appropriate response feat(route.ts): add additional error handling in GET function to return a 500 status code response when an error occurs during data fetching

--- src/app/api/music/now-playing/route.ts
+++ src/app/api/music/now-playing/route.ts
@@ -3,30 +3,33 @@ import { NextResponse } from "next/server";
 
 const NOW_PLAYING_ENDPOINT = 'https://api.spotify.com/v1/me/player/currently-playing';
 
-const playingEndpoint = async (): Promise<Response> => {
+const playingEndpoint = async (): Promise<any> => {
   const accessToken = await getAccessToken();
-  return fetch(NOW_PLAYING_ENDPOINT, {
+  const data = await fetch(NOW_PLAYING_ENDPOINT, {
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
     cache: 'no-cache',
   });
+
+  if (!data.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return data;
 }
 
 export const GET = async(): Promise<NextResponse> => {
-  const data = await playingEndpoint();
-  const track = await data.json();
-
-  console.log(track);
-
   try {
+    const data = await playingEndpoint();
+    const track = await data.json();
+    
     if (!track || !track.is_playing) {
       return NextResponse.json({ error: "No track playing" }, { status: 404 });
     }
 
     return NextResponse.json(track);
   } catch (error) {
-    console.error(error);
     return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
   }
 }
