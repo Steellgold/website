d6d5bdf5c118f17a2c3d293b94f6b58f1e874db0
Gaëtan H <gaetanhus@gmail.com>
2024-11-10 16:47:47 +0100

    fix(page.tsx): update URLs and add keywords in metadata for better SEO and clarity

--- src/app/(apps-in-app)/2048/page.tsx
+++ src/app/(apps-in-app)/2048/page.tsx
@@ -5,15 +5,16 @@ export const metadata: Metadata = {
   title: "2048 | Gaëtan's Portfolio",
   applicationName: "Gaëtan's Portfolio",
   description: "2048 game made for Melvynx's challenge.",
+  keywords: ["2048", "game", "challenge", "melvynx", "Melvyn", "Malherbe", "melvyn"],
   openGraph: {
     title: "2048 | Gaëtan's Portfolio",
     description: "2048 game made for Melvynx's challenge.",
     type: "website",
-    url: "https://gaetanhus.fr",
+    url: "https://steellgold.fr/2048",
     siteName: "2048 | Gaëtan's Portfolio",
     images: [
       {
-        url: "https://gaetanhus.fr/2048-og.png",
+        url: "https://steellgold.fr/2048-og.png",
         width: 1200,
         height: 630,
         alt: "Gaëtan's Portfolio",
@@ -23,7 +24,7 @@ export const metadata: Metadata = {
   twitter: {
     images: [
       {
-        url: "https://gaetanhus.fr/2048-og.png",
+        url: "https://steellgold.fr/2048-og.png",
         width: 1200,
         height: 630,
         alt: "Gaëtan's Portfolio",
