472d43f5a5900eb1de4cd31927ede17bb20cb0c2
Gaëtan H <gaetanhus@gmail.com>
2024-12-15 17:00:31 +0100

    feat(Buttons): add Quizz link and improve button visibility options

--- src/lib/components/buttons.tsx
+++ src/lib/components/buttons.tsx
@@ -5,13 +5,18 @@ import { useLang } from "../stores/lang.store";
 import { useViewMode } from "../stores/mode.store";
 import { Button } from "./ui/button";
 import { FaSpotify } from "react-icons/fa";
-import { ArrowRight, ArrowUpRight, Binary, Ellipsis, Files, FilesIcon, GraduationCap, Grid3X3 } from "lucide-react";
+import { ArrowRight, ArrowUpRight, Binary, Ellipsis, Files, FilesIcon, GraduationCap, Grid3X3, Shapes } from "lucide-react";
 import { ThemeSwitcher } from "./theme-switcher";
 import React, { ReactElement } from "react";
 
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Component } from "./utils/component";
 
-export const Buttons = (): ReactElement => {
+type Props = {
+  showTopButton?: boolean;
+};
+
+export const Buttons: Component<Props> = ({ showTopButton = true }) => {
     const { viewMode, setViewMode } = useViewMode();
     const { lang, setLang } = useLang();
 
@@ -26,7 +31,7 @@ export const Buttons = (): ReactElement => {
         </DropdownMenuTrigger>
         <DropdownMenuContent>
           <DropdownMenuItem asChild>
-            <Link href="/blog">
+            <Link href="/blog" className="cursor-pointer">
               <Files size={16} />
               <span className="ml-2">Blog</span>
             </Link>
@@ -35,21 +40,29 @@ export const Buttons = (): ReactElement => {
           <DropdownMenuSeparator />
 
           <DropdownMenuItem asChild>
-            <Link href="/2048">
+            <Link href="/quizz" className="cursor-pointer">
+              <Shapes size={16} />
+              <span className="ml-2">Quizz</span>
+            </Link>
+          </DropdownMenuItem>
+
+
+          <DropdownMenuItem asChild>
+            <Link href="/2048" className="cursor-pointer">
               <Grid3X3 size={16} />
               <span className="ml-2">2048</span>
             </Link>
           </DropdownMenuItem>
 
           <DropdownMenuItem asChild>
-            <Link href="/guess">
+            <Link href="/guess" className="cursor-pointer">
               <Binary size={16} />
               <span className="ml-2">Guess</span>
             </Link>
           </DropdownMenuItem>
 
           <DropdownMenuItem asChild>
-            <Link href="/spotiview">
+            <Link href="/spotiview" className="cursor-pointer">
               <FaSpotify size={16} />
               <span className="ml-2">Spotiview</span>
             </Link>
@@ -73,15 +86,17 @@ export const Buttons = (): ReactElement => {
         </DropdownMenuContent>
       </DropdownMenu>
 
-      <Button
-        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-        variant="outline"
-        size="sm"
-        className="flex items-center gap-1"
-      >
-        <ArrowUpRight size={16} />
-        <span className="hidden sm:block">Top</span>
-      </Button>
+      {showTopButton && (
+        <Button
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          variant="outline"
+          size="sm"
+          className="flex items-center gap-1"
+        >
+          <ArrowUpRight size={16} />
+          <span className="hidden sm:block">Top</span>
+        </Button>
+      )}
     </div>
   )
 }
