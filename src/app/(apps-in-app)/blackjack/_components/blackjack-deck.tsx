727cba99aca314ee9ef6bc99b4d6d87b064378f9
Gaëtan Huszovits-Reslinger <gaetan.huszovits-reslinger@uha.fr>
2024-12-19 15:47:24 +0100

    feat(blackjack-deck): add hover effect to cards and improve card display logic refactor(blackjack-deck): update card component to include isReloadCard prop for better state management

--- src/app/(apps-in-app)/blackjack/_components/blackjack-deck.tsx
+++ src/app/(apps-in-app)/blackjack/_components/blackjack-deck.tsx
@@ -4,24 +4,29 @@ import { useLang } from "@/lib/stores/lang.store";
 import { useBlackjack } from "../_lib/hook/use-blackjack";
 import { BlackjackCard } from "./blackjack-card";
 import { BlackjackCard as UIBlackjackCard } from "./ui/blackjack-card";
+import { useState } from "react";
 
 export const BlackjackDeck = () => {
   const { deck } = useBlackjack();
   const { lang } = useLang();
 
+  const [isHover, setIsHover] = useState(false);
+
   return (
     <UIBlackjackCard className="absolute top-1/2 transform -translate-y-1/2 right-5">
-      <div className="relative w-24 h-36 rounded-md">
+      <div
+        className="relative w-24 h-36 rounded-md"
+        onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
         {deck.length >= 1 ? deck.map((card, index) => (
           <div
             key={index}
-            className="absolute"
+            className="absolute transition-transform duration-300 ease-in-out"
             style={{
-              transform: `translateY(${index * - 3.5}px)`,
+              transform: isHover ? `translateY(${index * - 3.5}px)` : `translateY(${index * - 1}px)`,
               zIndex: index,
             }}
           >
-            <BlackjackCard rank={card.rank} suit={card.suit} isHidden />    
+            <BlackjackCard rank={card.rank} suit={card.suit} isHidden isReloadCard={card.isReloadCard} />
           </div>
         )) : (
           <div className="select-none">
@@ -31,6 +36,10 @@ export const BlackjackDeck = () => {
           </div>
         )}
       </div>
+
+      {/* <p className="absolute bottom-0 right-0 text-xs text-gray-400">
+        {deck.length} {lang === "fr" ? "cartes" : "cards"}
+      </p> */}
     </UIBlackjackCard>
   );
 }
