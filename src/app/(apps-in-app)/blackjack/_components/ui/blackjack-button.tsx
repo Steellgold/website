5d8bb940554a25dcf1da970a72b3f2df74dff8dc
Gaëtan H <gaetanhus@gmail.com>
2024-12-21 00:12:41 +0100

    feat(blackjack-button): add 'small' size option to BlackjackButton for better UI flexibility feat(blackjack-input): create new BlackjackInput component for user input in the blackjack app

--- src/app/(apps-in-app)/blackjack/_components/ui/blackjack-button.tsx
+++ src/app/(apps-in-app)/blackjack/_components/ui/blackjack-button.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 import React, { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 
 type BlackjackButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
-  size?: "default" | "icon";
+  size?: "default" | "icon" | "small";
 }
 
 export const BlackjackButton: Component<BlackjackButtonProps> = ({ children, className, size = "default", ...props }) => {
@@ -18,6 +18,7 @@ export const BlackjackButton: Component<BlackjackButtonProps> = ({ children, cla
       "disabled:bg-opacity-10 disabled:border-opacity-20 disabled:text-opacity-50 disabled:cursor-not-allowed",
       className, {
         "px-3 py-1": size === "default",
+        "px-2 py-1": size === "small",
         "px-0.5 py-0.5": size === "icon"
       }
     )} {...props}>
