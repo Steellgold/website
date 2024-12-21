78ee3b35cdbc893b52590337c597903948ef3d3a
Gaëtan H <gaetanhus@gmail.com>
2024-12-21 15:22:02 +0100

    chore(blackjack): remove unused identity store and hook to clean up codebase

--- src/app/(apps-in-app)/blackjack/_lib/hook/use-identity.ts
+++
@@ -1,6 +0,0 @@
-import { useIdentityStore } from './store/use-identity.store';
-
-export const useIdentity = () => {
-  const { name, setName, isHost, setIsHost, hydrated } = useIdentityStore();
-  return { name, setName, isHost, setIsHost, hydrated };
-};
