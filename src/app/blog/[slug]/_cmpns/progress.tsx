9cd39a10635c216944e45f76c21d30aec55ca3d3
Gaëtan H <me@gaetanhus.fr>
2025-02-13 15:41:48 +0100

    feat(blog): remove background color from ProgressBar for improved visibility

--- src/app/blog/[slug]/_cmpns/progress.tsx
+++ src/app/blog/[slug]/_cmpns/progress.tsx
@@ -22,7 +22,7 @@ const ProgressBar = (): ReactElement => {
   }, []);
 
   return (
-    <div className="fixed top-0 left-0 w-full h-1 bg-primary/20 z-50">
+    <div className="fixed top-0 left-0 w-full h-1 z-50">
       <div 
         className="h-full bg-white transition-all duration-150 ease-out"
         style={{ width: `${progress * 100}%` }}
