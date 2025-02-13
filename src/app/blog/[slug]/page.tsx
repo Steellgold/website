b1e09997df80fe45cac4130ad8c81e4575730bd6
Gaëtan H <me@gaetanhus.fr>
2025-02-13 15:32:56 +0100

    feat(blog): restructure Post component layout for improved readability and organization

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -9,7 +9,7 @@ import { BackToBlogButton } from "../_components/back-blog";
 import { ConfettiReadComponent } from "../_components/confetti-readed";
 import { ShareThisArticle } from "./_cmpns/share";
 import { ImageZoomer } from "image-zoomer-react";
-import { TableOfContents } from "./_cmpns/toc";
+import TableOfContents from "./_cmpns/toc";
 
 type PageProps = {
   params: Promise<{
@@ -98,36 +98,37 @@ const Post: AsyncComponent<PageProps> = async props => {
   return (
     <>
       <BackToBlogButton onlyButton />
-
       <ConfettiReadComponent />
 
-      <article className="max-w-4xl mx-auto px-4 py-8">
-        <ImageZoomer
-          src={data.banner}
-          alt="illustration image"
-          width={800}
-          height={400}
-          className="rounded-lg mb-8"
-        />
-
-        <header className="flex flex-col mb-8 items-center">
-          <h1 className="text-3xl font-bold text-center">{data.title}</h1>
-          <div className="flex items-center justify-center space-x-4">
-            <span className="flex items-center">
-              <CalendarIcon className="w-4 h-4 mr-2" />
-              {dayJS(data.createdAt).format("MMMM D, YYYY")}
-            </span>
-          </div>
-
-          <ShareThisArticle slug={data.slug} title={data.title} />
-        </header>
-
+      <div className="relative">
         <TableOfContents content={data.content} />
+        
+        <article className="max-w-4xl mx-auto px-4 py-8">
+          <ImageZoomer
+            src={data.banner}
+            alt="illustration image"
+            width={800}
+            height={400}
+            className="rounded-lg mb-8"
+          />
 
-        <div className="prose prose-invert prose-lg max-w-none">
-          <MarkdownPlease content={data.content} />
-        </div>
-      </article>
+          <header className="flex flex-col mb-8 items-center">
+            <h1 className="text-3xl font-bold text-center">{data.title}</h1>
+            <div className="flex items-center justify-center space-x-4">
+              <span className="flex items-center">
+                <CalendarIcon className="w-4 h-4 mr-2" />
+                {dayJS(data.createdAt).format("MMMM D, YYYY")}
+              </span>
+            </div>
+
+            <ShareThisArticle slug={data.slug} title={data.title} />
+          </header>
+
+          <div className="prose prose-invert prose-lg max-w-none">
+            <MarkdownPlease content={data.content} />
+          </div>
+        </article>
+      </div>
     </>
   );
 }
