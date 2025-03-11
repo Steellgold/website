97750b2ebe32e5eee504e3cc4ba979f2ba37f0b4
Gaëtan H <gaetanhus@gmail.com>
2025-03-11 22:06:19 +0100

    feat(privacy, terms): add Privacy and Terms pages with basic content to inform users about data policies

---
+++ src/app/(legal)/terms/page.tsx
@@ -0,0 +1,12 @@
+const PrivacyPage = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>
+        This is the privacy policy for our app. We do not collect any personal data.
+      </p>
+    </div>
+  )
+}
+
+export default PrivacyPage;
