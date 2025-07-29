9bf827cea1da8f3d59bae176a2bf05f460e83a5f
Gaëtan H <gaetanhus@gmail.com>
2025-07-29 15:13:19 +0200

    Revamp NotFound component for improved user experience
    - Enhanced the layout and typography of the NotFound page with new font configurations for better visual appeal.
    - Updated the structure to include a prominent 404 header and a more engaging message.
    - Improved navigation with a styled link back to the home page, featuring an icon for better user interaction.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,12 @@
 'use client';
 
 import { useAppContext } from '@/contexts/app-context';
+import { piano, piano_electric } from '@/lib/font';
+import { cn } from '@/lib/utils';
+import { ArrowLeftIcon } from 'lucide-react';
 import Link from 'next/link';
 import { useEffect } from 'react';
- 
+
 const NotFound = () => {
   const { setIs404 } = useAppContext();
 
@@ -13,20 +16,38 @@ const NotFound = () => {
   }, [setIs404]);
 
   return (
-    <div className="flex flex-col items-center justify-center relative h-screen">
-      <h2 className="text-3xl font-bold">
-        You&apos;re lost?
-      </h2>
+    <div className="flex flex-col items-center justify-center min-h-screen px-4 text-center">
+      <div className="mb-8">
+        <h1 className={cn(
+          "text-8xl sm:text-9xl font-bold text-white",
+          piano_electric.className
+        )}>
+          404
+        </h1>
+      </div>
 
-      <p className="mb-4">
-        The page you are looking for does not exist.
-      </p>
+      <div className="max-w-md mb-2">
+        <h2 className={cn("text-2xl sm:text-3xl font-bold", piano.className )}>
+          Oops !
+        </h2>
+        
+        <p className="text-muted-foreground text-lg leading-relaxed">
+          The page you are looking for does not exist.
+        </p>
+      </div>
 
-      <Link href="/" className="">
-        Go back home
-      </Link>
+      <div className="flex flex-col sm:flex-row gap-4 w-full max-w-sm justify-center">
+        <Link 
+          href="/" 
+          className="text-white underline flex flex-row items-center gap-2 hover:text-gray-300 transition-colors"
+          prefetch
+        >
+          <ArrowLeftIcon className="w-4 h-4" />
+          Back to home
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default NotFound;
