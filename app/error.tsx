80f9d3c77a0fe4bcd3d00e4f36d084edab24d838
Gaëtan H <gaetanhus@gmail.com>
2025-09-10 20:12:10 +0200

    Add ErrorPage component to handle error display and reset functionality

---
+++ app/error.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { Button } from '@/components/ui/button';
+import { useAppContext } from '@/contexts/app-context';
+import { piano, piano_electric } from '@/lib/font';
+import { cn } from '@/lib/utils';
+import { RefreshCcw } from 'lucide-react';
+import { useEffect } from 'react';
+
+type ErrorPageProps = {  
+  error: Error & { digest?: string }
+  reset: () => void
+}
+
+const ErrorPage = ({ error, reset }: ErrorPageProps) => {
+  const { setIs404 } = useAppContext();
+
+  useEffect(() => {
+    setIs404(true);
+    return () => setIs404(false);
+  }, [setIs404]);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen px-4 text-center">
+      <div className="mb-8">
+        <h1 className={cn(
+          "text-8xl sm:text-9xl font-bold text-white",
+          piano_electric.className
+        )}>
+          Oops !
+        </h1>
+      </div>
+
+      <div className="max-w-md mb-2">
+        <h2 className={cn("text-2xl sm:text-3xl font-bold", piano.className )}>
+          Something went wrong!
+        </h2>
+        
+        <p className="text-muted-foreground text-lg leading-relaxed">
+          An unexpected error has occurred. Please try again later.
+        </p>
+      </div>
+
+      <div className="flex flex-col sm:flex-row gap-4 w-full max-w-sm justify-center">
+        <Button variant={"outline"}>
+          <RefreshCcw size={16} />
+          <span onClick={() => reset()}>Try again</span>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
