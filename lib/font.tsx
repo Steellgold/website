b53c1f850e19015f00ffd10f9c68d25fd3db7136
Gaëtan H <me@gaetanhus.fr>
2025-09-12 13:51:48 +0200

    Update font style in font.tsx from italic to normal for piano export

--- lib/font.tsx
+++ lib/font.tsx
@@ -2,7 +2,7 @@ import { Instrument_Serif } from "next/font/google";
 
 export const piano = Instrument_Serif({
   weight: "400",
-  style: "italic",
+  style: "normal",
   subsets: ["latin"],
 });
 
