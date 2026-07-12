2bfdbbf6194bf9a8d9e9b500f61815d371557e25
Gaëtan H <me@gaetanhus.fr>
2026-07-12 02:13:48 +0200

    chore: remove old website codebase

--- i18n/routing.ts
+++
@@ -1,4 +0,0 @@
-export const locales = ['fr', 'en'] as const;
-export type Locale = (typeof locales)[number];
-export const defaultLocale: Locale = 'fr';
-
--- lib/blog.ts
+++
@@ -1,3 +0,0 @@
-import { SimplistClient } from "@simplist.blog/sdk";
-
-export const blog = new SimplistClient()
