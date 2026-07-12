17272cdba2ffea10ee98b5859f55fce87aecca80
Gaëtan H <me@gaetanhus.fr>
2026-07-12 02:14:15 +0200

    build(config): add project configuration

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+import path from "path";
+
+const withNextIntl = createNextIntlPlugin("./i18n.ts");
+
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: path.join(__dirname),
+  },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "cdn.gaetanhus.fr", port: "", pathname: "/**" },
+    ],
+  },
+};
+
+export default withNextIntl(nextConfig);
